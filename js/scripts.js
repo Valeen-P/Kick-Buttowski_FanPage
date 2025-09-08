852b91de0158be69abdb11ac2ecda3cb054fd5a2
Valeen-P <vperez226@alumnos.frh.utn.edu.ar>
2025-09-08 18:49:59 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollSections = document.querySelectorAll('.scroll-section');
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    scrollSections.forEach(section => {
+        observer.observe(section);
+    });
+
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (hamburger && navLinks) {
+        hamburger.addEventListener('click', () => {
+            navLinks.classList.toggle('show');
+        });
+    }
+});
